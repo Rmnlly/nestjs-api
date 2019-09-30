7c23781f254a147371338513462010259012159b
Raman <33904740+Rmnlly@users.noreply.github.com>
2019-09-29 21:29:24 -0400

    started to add some basic descriptions on which parts do what in nest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+//this will handle your-domain/, we're handling the route for home
+//we would need a method in here to handle other routes like @Controller("/hair-styles") to handle that route
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //This decorator @Get is fired when a get request is made to your-domain/, but if we did your-domain/hair this isn't fired
+  //If uptop we called @Controller("hair") and set this to @Get("styles") the get would handle requests for /hair/styles
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], //handling incoming requests (sending responses as well)
   providers: [AppService],
+  //these provide certain functionality (like reaching into a db to get data, then the controlling calls this service)
+  //This can keep out controlers clean, not needing to have direct access to the db
 })
 export class AppModule {}
