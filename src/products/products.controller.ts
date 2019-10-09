b2067e8cc9b67f8012432171eeaedfb6e4deed5a
Raman <33904740+Rmnlly@users.noreply.github.com>
2019-10-08 21:56:14 -0400

    Completed single product lookup, update and delete

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -22,5 +30,27 @@ export class ProductsController {
   @Get()
   getAllProducts() {
     return this.productsService.fetchProducts();
+  }
+
+  @Get(':id')
+  //id is a dynamic segment like in express, so calling products/1 will make id be 1
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getASingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string) {
+    this.productsService.deleteProduct(prodId);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
@@ -6,7 +6,8 @@ export class ProductsService {
   products: Product[] = [];
 
   insertProduct(title: string, description: string, price: number) {
-    const prodId = new Date().toString();
+    // const prodId = new Date().toString();
+    const prodId = Math.random().toString(); //not perfect, just as a proof
     const newProduct = new Product(prodId, title, description, price);
     this.products.push(newProduct);
     return prodId;
@@ -17,5 +18,41 @@ export class ProductsService {
     //We don't want to return a direct reference to products
     //So we create a copy of the products
     //We could map over the individual products to make sure they can't be altered later on
+  }
+
+  getASingleProduct(productId: string) {
+    const [product] = this.findProduct(productId);
+    return { ...product }; //Creating a new object with new key/value pairs
+  }
+
+  updateProduct(
+    productId: string,
+    title: string,
+    description: string,
+    price: number,
+  ) {
+    const [product, index] = this.findProduct(productId);
+    //Setting the values to overwrite our selected prodct if they exist, if they don't then we short circuit
+    const updateProduct = {
+      ...product,
+      ...(title && { title }),
+      ...(description && { description }),
+      ...(price && { price }),
+    };
+    this.products[index] = updateProduct;
+  }
+
+  deleteProduct(productId: string) {
+    const [_, index] = this.findProduct(productId);
+    this.products = this.products.filter((prod, i) => i !== index);
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex(prod => prod.id === id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException(`could not find the product for id ${id}`);
+    }
+    return [product, productIndex];
   }
 }
